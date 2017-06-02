6ab1661e2f6781ae219ba65f7981667ad4ed4043
Martchus <martchus@gmx.net>
2017-06-02 17:21:08 +0200

    Handle win64 in dumpcpp and MetaObjectGenerator::readClassInfo
    See https://bugreports.qt.io/browse/QTBUG-46827

--- tools/dumpcpp/main.cpp
+++ tools/dumpcpp/main.cpp
@@ -1539,6 +1539,10 @@ int main(int argc, char **argv)
             typeLib = settings.value(QLatin1Char('/') + codes.at(c) + QLatin1String("/0/win32/.")).toString();
             if (QFile::exists(typeLib))
                 break;
+            typeLib = settings.value(QLatin1String("/") + codes.at(c) + QLatin1String("/0/win64/.")).toByteArray();
+            if (QFile::exists(typeLib)) {
+                break;
+            }
         }
 
         if (!typeLib.isEmpty())
@@ -1598,6 +1602,10 @@ int main(int argc, char **argv)
                                      + QLatin1String("/win32/.")).toString();
             if (QFile::exists(typeLib))
                 break;
+            typeLib = settings.value(key + QLatin1String("/") + codes.at(c) + QLatin1String("/win64/.")).toByteArray();
+            if (QFile::exists(typeLib)) {
+                break;
+            }
         }
     }
 
